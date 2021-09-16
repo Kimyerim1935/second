ca63fe5609746066ae6b6b612025130e0ab62dab
[yerim] <yerimhi@localley.com>
2021-09-16 19:31:42 +0900

    loading comp

---
+++ Loading.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Loading() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>Getting the wonderful weather</Text>
+        </View>
+    )
+        
+}
+
+const styles = StyleSheet.create({
+    container : {
+        flex : 1,
+        justifyContent : 'flex-end',
+        paddingHorizontal : 20,
+        paddingVertical : 100,
+        backgroundColor : 'royalblue'
+    },
+    text : {
+        color : 'white',
+        fontSize : 30,
+    }
+})
